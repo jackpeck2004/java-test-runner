478b47f4bdd5195b04c89be6a032b4d4494b4487
Giacomo Pasin <giacomo.pasin@gmail.com>
2023-09-11 19:02:04 +0200

    Initialized Spring Boot application as a web server

---
+++ src/main/java/com/giacomopasin/javatestrunner/JavaTestRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.giacomopasin.javatestrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestRunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giacomopasin/javatestrunner/JavaTestRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giacomopasin.javatestrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
